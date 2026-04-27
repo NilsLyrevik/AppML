f037f5a4106f3f3ae0cb8ab2f1f0e389561286e4
NilsLyrevik <karlnilslyrevik@gmail.com>
2026-04-27 16:39:12 +0200

    hello

--- PA3/classifier.py
+++ PA3/classifier.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import html
-from sklearn.metrics import cohen_kappa_score
+from sklearn.metrics import cohen_kappa_score, accuracy_score
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.linear_model import LogisticRegression
@@ -31,10 +31,34 @@ def test_train_split(X, y, test_size=0.2):
     (X_train, X_test, y_train, y_test) = train_test_split(X, y, test_size=test_size, random_state=1337)
     return X_train, X_test, y_train, y_test
 
-def agreement_score(crowd, gold):
-    kappa = cohen_kappa_score(crowd["sentiment"], gold["sentiment"])
+def agreement_accuracy_score(crowd, gold):
+    df = crowd.merge(gold, on="text", suffixes=("_crowd", "_gold"))
 
-    print(kappa)
+    print("Aligned rows:", len(df))
+
+    kappa = cohen_kappa_score(
+        df["sentiment_crowd"],
+        df["sentiment_gold"]
+    )
+
+    acc = accuracy_score(
+        df["sentiment_crowd"],
+        df["sentiment_gold"]
+    )
+
+    return (kappa,acc)
+
+
+
+def feature_processing(cs):
+        # replace twitter handles with @USER
+    cs["text"] = cs["text"].str.replace(r"\B@\w+", "@USER", regex=True)
+
+    # fix html unicode (&amp; -> &, &lt; -> <, etc.)
+    cs["text"] = cs["text"].apply(html.unescape)
+
+    # remove URLs
+    cs["text"] = cs["text"].str.replace(r"http\S+|www\.\S+", "", regex=True)
 
 def main():
     cs_train = pd.read_csv("data/crowdsourced_train.csv", sep="\t")
@@ -42,23 +66,20 @@ def main():
 
     # normalize sentiment labels
     cs_train["sentiment"] = cs_train["sentiment"].str.lower().str.strip()
+    gold_train["sentiment"] = gold_train["sentiment"].str.lower().str.strip()
 
     # keep only valid labels
     valid_labels = ["positive", "negative", "neutral"]
     cs_train = cs_train[cs_train["sentiment"].isin(valid_labels)]
+    gold_train = gold_train[gold_train["sentiment"].isin(valid_labels)]
 
-    # replace twitter handles with @USER
-    cs_train["text"] = cs_train["text"].str.replace(r"\B@\w+", "@USER", regex=True)
-
-    # fix html unicode (&amp; -> &, &lt; -> <, etc.)
-    cs_train["text"] = cs_train["text"].apply(html.unescape)
-
-    # remove URLs
-    cs_train["text"] = cs_train["text"].str.replace(r"http\S+|www\.\S+", "", regex=True)
-
-    print(cs_train)
-
-    agreement_score(cs_train, gold_train)
+    feature_processing(cs_train)
+    feature_processing(gold_train)
+    
+    agr_score, acc_score = agreement_accuracy_score(cs_train,gold_train)
+    print("Agreement score (kappa) is: ",agr_score)
+    print("Accuracy score is: ",acc_score)
+    
     
 
 if __name__ == "__main__":
